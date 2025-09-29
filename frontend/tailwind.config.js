9c9e4dafbf67cecaf6bbde8d7c309e6ad433bdd3
RushaanRajeshPai <pairushaan64@gmail.com>
2025-09-30 00:42:27 +0530

    frontend setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",  // important for TSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
